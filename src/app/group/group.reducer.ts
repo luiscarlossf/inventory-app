292d2b4e8df28547c58c75cdb92689b41f8cbe47
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-15 17:01:38 -0300

    Adiciona Group Reducer

---
+++ src/app/group/group.reducer.ts
@@ -0,0 +1,21 @@
+import { Group } from './group.model';
+import { Action } from 'redux';
+
+/**Definição do estado do ramo groups da árvore state*/
+export interface GroupsState {
+    group: Array<Group>;
+};
+
+/**Estado inicial para groups*/
+const initialState = {
+    group: null
+};
+
+/**Reducer do ramo groups da árvore state*/
+export const GroupsReducer =
+function (state: GroupsState=initialState, action: Action): GroupsState {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
