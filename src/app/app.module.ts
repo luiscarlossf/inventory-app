eff826ce9038f67e4addd24f01f69fb99ff9a028
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-18 18:02:34 -0300

    Adiciona os providers Auth, Backend e httpInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
+import { httpInterceptorProviders } from './http-interceptors/index';
+import { BackendService } from './backend.service';
 
 @NgModule({
   declarations: [
@@ -13,9 +17,14 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    BackendService,
+    httpInterceptorProviders,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
