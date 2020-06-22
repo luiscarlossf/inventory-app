0aa29c65704b2ee9da30407b98a2d73c33241c3b
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-22 16:11:35 -0300

    Adiciona backendService.

---
+++ src/app/backend.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * Serviço que realiz a comunicação com o back-end do sistema.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+
+  constructor() { }
+}
