0671f4bd803c60a261adae2d78804c24b8156254
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-24 19:08:49 -0300

    Altera o serviço de autenticação.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { User } from './user/user.model';
 import { BackendService } from './backend.service';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Store, select } from '@ngrx/store';
+import * as fromUser from './user/user.reducer';
 
 /**
  * Serviço responsável pela autenticação dos usuários do sistema.
@@ -8,16 +13,30 @@ import { BackendService } from './backend.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  /**@type {User} Usuário corrente logado. */
+export class AuthService implements OnInit{
+  /**@type {object} Stream do ramo da árvore de estado da aplicação. */
+  private user$: Observable<fromUser.UserState>;
+  /**@type  {User} Usuário logado no sistema.*/
   private user: User;
   /**@type {boolean} Indica o progresso do processo de login. */
   private loading: boolean;
   /**@type {string} Token de autorização do usuário corrente. */
   private token: string;
+  /**@type {string} Url da Backend API */
+  private apiUrl:  string = environment.apiUrl;
+  /**@type {string} Url da página a ser redirecionada caso o login tenha sucesso.*/
+  redirectUrl: string;
 
-  constructor(private backend: BackendService) { 
+  constructor(private http: HttpClient, private readonly store: Store<{user: fromUser.UserState}>) { 
+    this.user$ = store.pipe(select('user')) ;
+  }
 
+  ngOnInit(){
+    this.user$.subscribe(
+      user => {
+        this.setUser(user.currentUser);
+        this.setToken(user.currentUser.token);
+    });
   }
 
   /**
@@ -53,18 +72,35 @@ export class AuthService {
   }
 
   /**
-   * Realiza o login do usuário representado por `data`.
-   * @param {any} data Objecto com o email e senha do usuário a ser cadastrado.
+   * Verifica se o existe algum usuário logado.
    */
-  login(data: any): void{
-    this.loading = true;
+  isLoggedIn(): boolean{
+    if(this.user){
+      return true;
+    }else{
+      return false;
+    }
   }
 
   /**
-   * Faz o logout do usuário corrente.
+   * Realiza o login do usuário representado por `data`.
+   * @param {any} data Objeto com o email e senha do usuário a ser cadastrado.
    */
-  logout(): void{
-    this.user = null;
-    this.token = null;
+  login(data: any): Observable<User>{
+    console.log("Realizando login");
+    this.loading = true;
+    let options = {
+      headers : {'Content-Type':  'application/json'},
+      observe: 'body' as const,
+      params: null,
+      reportProgress: true,
+      responseType: 'json' as const,
+      withCredentials: false,
+    };
+    let credentials = JSON.stringify(data);
+    //////////////////////////////
+    console.log(credentials);
+    /////////////////////////////
+    return this.http.post<User>(this.apiUrl, credentials, options);
   }
 }
