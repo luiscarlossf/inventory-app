e78335dc5ebe1ec477931c147c39a952981c7e9f
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-24 19:04:10 -0300

    Define o UserReducer.

--- src/app/user/user.reducer.ts
+++ src/app/user/user.reducer.ts
@@ -1,33 +1,69 @@
 import { User } from './user.model';
 import * as UserActions from './user.actions';
-import { Action } from 'redux';
-import { BrowserStack } from 'protractor/built/driverProviders';
+import { createReducer, Action, on, createSelector } from '@ngrx/store';
 
 /**
  * @interface
  */
-export interface UsersState{
+export interface UserState{
     /**@type {object} Usuário logado no sistema.*/
     currentUser: User;
+    keepConnected: boolean;
+    error: any;
 };
 
 /** Estado inicial para users */
-const initialState: UsersState= {
-    currentUser: null
+const initialState: UserState= {
+    currentUser: null,
+    keepConnected: null,
+    error: null
 };
 
-/**Reducer do ramo usuários. */
-export const UsersReducer = 
-function (state: UsersState = initialState, action: Action): UsersState {
-    switch(action.type){
-        case UserActions.SET_CURRENT_USER:
-            const user: User = (<UserActions.SetCurrentUserAction> action).user;
-            return {
-                currentUser: user
-            };
-            break;
+/**
+ * Cria a função reducer que manipula as transações
+ * de estado do branch users de maneira imutável.
+*/
+const userReducer = createReducer(
+    initialState,
+    on(UserActions.login, (state, { email, password, connected }) => (state)),
+    on(UserActions.loginSuccess, (state, { user })=> ({...state, currentUser: user, error: null})),
+    on(UserActions.loginFailure, (state, { error })=> ({...state, currentUser: null, error: error})),
+    on(UserActions.logout, state => ({
+        currentUser:null,
+        keepConnected: null,
+        error: null
+    })),
+    on(UserActions.keepConnected, state => ({...state, keepConnected: true}))
+);
 
-        default:
-            return state;
+/**
+ * Note: The exported reducer function is necessary as 
+ * function calls are not supported by the AOT compiler 
+ */
+export function reducer(state: UserState | undefined, action: Action){
+    return userReducer(state, action);
+}
+
+/**
+ * Seleciona o usuário corrente do ramo user.
+ */
+export const selectUser =  (state: UserState) => state.currentUser;
+
+/**
+ * Seleciona o erro do ramo user.
+ */
+export const selectError =  (state: UserState) => state.error;
+
+/**
+ * Seleciona a propriedade keepConnected do ramo user.
+ */
+export const selectKeepConnected =  (state: UserState) => state.keepConnected;
+
+/**Retorna o token do usuário corrente */
+export const getToken = createSelector(
+    selectUser,
+    (selectedUser: User) => {
+        return selectedUser.token;
     }
-};
+);
+
