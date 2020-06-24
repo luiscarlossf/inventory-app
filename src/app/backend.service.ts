0768794d4efa121501046152f20af526acb0a201
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-24 19:08:30 -0300

    Altera a definição do serviço backend.

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable, EMPTY } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { catchError, tap, map } from 'rxjs/operators';
+import { User } from './user/user.model';
 
 /**
  * Serviço que realiz a comunicação com o back-end do sistema.
@@ -8,5 +13,8 @@ import { Injectable } from '@angular/core';
 })
 export class BackendService {
 
-  constructor() { }
+  apiUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
 }
