9aac22d4fbc0318040a2c2d300252e6f47643ae0
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-24 19:05:51 -0300

    Altera a classe do componente Login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, EmailValidator, Validators, CheckboxRequiredValidator } from '@angular/forms';
+import { Store, select } from '@ngrx/store';
+import * as UserActions from '../user/user.actions';
+import * as fromUser from '../user/user.reducer';
+import { Observable } from 'rxjs';
+import { User } from '../user/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,19 +14,32 @@ import { FormBuilder, FormGroup, EmailValidator, Validators, CheckboxRequiredVal
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-
-  constructor( fb: FormBuilder) { 
+  user$: Observable<fromUser.UserState>;
+  
+  constructor( fb: FormBuilder, private readonly store: Store<{user:fromUser.UserState}>, private router: Router) { 
     this.loginForm = fb.group({
       'email':[''],
-      'pwd':[''],
+      'password':[''],
       'checkbox':[false]
     });
+    this.user$ = store.pipe(select('user'));
   }
 
   ngOnInit(): void {
+    this.user$.subscribe(
+      user => {
+        console.log("User subscribe!");
+        if(user.currentUser){
+          this.router.navigate(['home']).then(()=> console.log('Lindo!'));
+        }else{
+          console.log("Erro no login");
+        }
+    });
   }
 
   onSubmit(form: any){
-    console.log("O valor do formulário é ", form);
+    let payload = {email: form.email, password:form.password, connected: form.checkbox};
+    console.log("O valor do formulário é ", payload);
+    this.store.dispatch(UserActions.login(form));
   }
 }
