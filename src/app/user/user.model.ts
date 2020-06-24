3f09abe6276e8cee4e5bb4d3073b9444a4db570e
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-24 19:04:40 -0300

    Adiciona a propriedade token.

--- src/app/user/user.model.ts
+++ src/app/user/user.model.ts
@@ -8,27 +8,29 @@ import { Permission } from '../permission/permission.model';
 export interface User{
     
     /** @type {string} Username de 150 ou menos caracteres.*/
-    username: string,
+    username: string;
     /** @type {string} Primeiro nome do usuário de 30 ou menos caracteres.*/
-    first_name?: string,
+    first_name?: string;
     /** @type {string} Sobrenome do usuário de 150 ou menos caracteres.*/
-    last_name?: string,
+    last_name?: string;
     /** @type {string} Email do usuário. Opcional.*/
-    email?: string,
+    email: string;
     /** @type {string} Senha da conta do usuário.*/
-    password: string,
+    password?: string;
+    /**@type {string} Token de autorização do usuário */
+    token: string;
     /** @type {Array<Group>} Grupos de permissões a qual o usuário pertence*/
-    groups: Array<Group>,
+    groups: Array<Group>;
     /** @type {Array<Permission>} Permissões do usuário.*/
-    user_permissions: Array<Permission>,
+    user_permissions: Array<Permission>;
     /** @type {boolean} Indica se o usuário pode acessar o site admin.*/
-    is_staff: boolean,
+    is_staff: boolean;
     /** @type {boolean} Indica se a conta do usuário pode ser considerada ativa.*/
-    is_active: boolean,
+    is_active: boolean;
     /** @type {boolean} Indica se o usuário tem todas as permissões sem atibuições explícitas a elas.*/
-    is_superuser: boolean,
+    is_superuser: boolean;
     /** @type {Date} Data e hora do último login do usuário.*/
-    last_login: Date,
+    last_login: Date;
     /** @type {Date} Data e hora de quando a conta do usuário foi criada.*/
-    data_joined: Date,
+    data_joined: Date;
 }
