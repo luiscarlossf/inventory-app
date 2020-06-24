8df2389faaeb15be50edb0de1f68797d0b483c51
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-06-24 19:07:29 -0300

    Altera o interceptor de autenticação.

--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -1,8 +1,10 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
+import { Injectable } from '@angular/core';
 
 /**Atribui o token de autorização do usuário logado em cada requisicação.*/
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor{
 
     constructor(private auth: AuthService){  }
@@ -10,10 +12,15 @@ export class AuthInterceptor implements HttpInterceptor{
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         const token = this.auth.getAuthorizationToken();
 
-        const authReq = req.clone({
-            headers: req.headers.set('Authorization', token),
-        });
+        if(token){
 
-        return next.handle(authReq);
+            const authReq = req.clone({
+                headers: req.headers.set('Authorization', `Token ${token}`),
+            });
+
+            return next.handle(authReq);
+        }else{
+            return next.handle(req);
+        }
     }
 }
