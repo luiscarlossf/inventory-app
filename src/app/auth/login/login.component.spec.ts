b9e3b50bd489c5b1abf9158e58ba2afd698f1c9d
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-07-02 19:04:01 -0300

    Altera o arquivo de especificação do component Login.

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,12 +9,14 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    TestBed.inject(FormBuilder);
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
