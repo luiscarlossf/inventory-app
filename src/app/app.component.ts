31c233aa58a1b30dad2f95a26326b1d4f7af9645
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-07-13 18:37:55 -0300

    Registra os ícones no serviço iconRegistry.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { User } from './user/user.model';
 
 @Component({
@@ -10,7 +12,32 @@ export class AppComponent {
   title = 'inventory-app';
   user: User;
 
-  constructor() {}
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'menu-icon',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/menu/menu-icon@1px.svg'));
+    iconRegistry.addSvgIcon(
+      'search-icon',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/search/search-icon@1px.svg'));
+    iconRegistry.addSvgIcon(
+      'logo-nav',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/logo/logo-nav@1px.svg'));
+    iconRegistry.addSvgIcon(
+      'apps',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/nav/apps.svg'));
+    iconRegistry.addSvgIcon(
+      'home',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/nav/home-minimal.svg'));
+    iconRegistry.addSvgIcon(
+      'logout',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/nav/logout-icon.svg'));  
+    iconRegistry.addSvgIcon(
+      'users',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/nav/multiple-19.svg'));
+    iconRegistry.addSvgIcon(
+      'chart',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/nav/chart-pie-36.svg'));
+  }
 
   doSearch(query: string){}
 
