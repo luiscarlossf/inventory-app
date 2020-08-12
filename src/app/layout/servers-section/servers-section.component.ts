841e916ced0399af4eb9f898d9f6cd6900bfefe8
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-08-12 15:24:19 -0300

    Adiciona o componete Servers Section.

---
+++ src/app/layout/servers-section/servers-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers-section',
+  templateUrl: './servers-section.component.html',
+  styleUrls: ['./servers-section.component.css']
+})
+export class ServersSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
