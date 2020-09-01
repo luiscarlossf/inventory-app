af8d6f349176cbc6456b26e571a7744a4c866d86
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-09-01 14:58:53 -0300

    Adiciona tela para iserção de novos equipamentos.

---
+++ src/app/components/insert-dialog/insert-dialog.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.state';
+
+@Component({
+  selector: 'app-insert-dialog',
+  templateUrl: './insert-dialog.component.html',
+  styleUrls: ['./insert-dialog.component.css']
+})
+export class InsertDialogComponent implements OnInit {
+  insertForm: FormGroup;
+
+  constructor( private readonly store: Store<AppState>, fb: FormBuilder) { 
+    this.insertForm = fb.group({
+      'patrimony': [''],
+      'brand': [''],
+      'category': [''],
+      'model': [''],
+      'ua': [''],
+      'andar': [''],
+      'warranty_start': [''],
+      'warranty_end': [''],
+      'acquisition_date': [''],
+      'acquisition_value': [''],
+      'policy': [''],
+      'status': [''],
+      'status_wsus': [''],
+      'status_trend': [''],
+      'status_zenworks': [''],
+
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
