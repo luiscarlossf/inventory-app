6538e76de94d87873b69303f6ca06baf1aee1f84
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-09-03 15:34:17 -0300

    Vincula equipamentos a tabela.

--- src/app/components/equipaments-table/equipaments-table.component.ts
+++ src/app/components/equipaments-table/equipaments-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import * as fromEquipament from '../../redux/equipament/equipament.reducer';
 import * as EquipamentActions from '../../redux/equipament/equipament.actions';
 import { MatTableDataSource } from '@angular/material/table';
@@ -10,26 +10,6 @@ import { AppState } from 'src/app/app.state';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatSort } from '@angular/material/sort';
 
-const dados: Equipament[] = [
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'10000000', brand:'Dell', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'20000000', brand:'ASUS', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'30000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'HP', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-  {patrimony:'00000000', brand:'Samsung', category:'Notebook', model:'SM809', ua:'Coordenadoria de TIC', floor:'2 ANDAR', status: Status.Usado , warranty_start: new Date(1997,1,14), warranty_end: new Date(2020,8,9), acquisition_date:new Date(2020,10,10), acquisition_value:2500},
-];
 @Component({
   selector: 'app-equipaments-table',
   templateUrl: './equipaments-table.component.html',
@@ -37,7 +17,7 @@ const dados: Equipament[] = [
 })
 export class EquipamentsTableComponent implements OnInit {
   dataSource : MatTableDataSource<Equipament>;
-  equipaments$: Observable<Array<Equipament>>;
+  @Input() equipaments$: Observable<Array<Equipament>>;
   columnsToDisplay = ['select', 'patrimony', 'brand', 'category', 'model', 'ua', 'floor','status', 'warranty_start','warranty_end', 'acquisition_date', 'acquisition_value'];
   initialSelection = [];
   allowMultiSelect = true;
@@ -46,17 +26,15 @@ export class EquipamentsTableComponent implements OnInit {
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(private readonly store: Store<AppState>){
-    //this.sub$ = of(dados);
-    this.dataSource = new MatTableDataSource<Equipament>(dados);
+    this.dataSource = new MatTableDataSource<Equipament>();
   }
 
   ngOnInit() {
+    this.equipaments$.subscribe( equipaments =>{
+      this.dataSource.data = equipaments;
+    });
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-    
-    //Vincula o ramo equipaments e carrega os equipamentos do servidor.
-    this.equipaments$ = this.store.pipe(select(fromEquipament.selectEquipaments));
-    this.store.dispatch(EquipamentActions.loadEquipaments());
   }
   
   /**
