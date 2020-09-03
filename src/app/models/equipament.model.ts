3c65eb2fe4e6ecf6d99e13092eb0ba075c138399
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-09-03 13:40:16 -0300

    Adiciona policy ao modelo Equipament.

--- src/app/models/equipament.model.ts
+++ src/app/models/equipament.model.ts
@@ -1,5 +1,5 @@
 
-export enum Status {'Usado', 'Almoxarifado', 'Sucata', 'Doação'};
+export enum Status {'Usado', 'Almoxarifado', 'Sucata', 'Doação', 'Estaleiro'};
 
 export interface Equipament{
     url?: string;
@@ -8,6 +8,7 @@ export interface Equipament{
     warranty_end?: Date;
     acquisition_date?: Date;
     acquisition_value?: number;
+    policy?:boolean;
     status: Status;
     /**@type {string} Url da marca do equipamento. */
     brand?: string;
