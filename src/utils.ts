f20318a98e567632e59716443305a99ce7e7bd76
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-09-25 16:20:25 -0300

    Adiciona um módulos de utilitários.

---
+++ src/utils.ts
@@ -0,0 +1,12 @@
+export const convertDateToString = (val: Date) => { 
+    let converted_date = val.getFullYear().toString() + '-' + val.getMonth().toString() + '-'+ val.getDay().toString();
+    console.log(converted_date);
+    return converted_date; 
+};
+
+export const convertStringToDate = (val: string) => { 
+    let splited_date = val.split('-');
+    let converted_date = new Date(parseInt(splited_date[0]), parseInt(splited_date[1]), parseInt(splited_date[2]));
+    console.log(converted_date);
+    return converted_date; 
+};
