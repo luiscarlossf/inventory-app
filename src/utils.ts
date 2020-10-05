12ede8d85a1b0515fa9d76e29309f1f74992f83b
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-10-05 15:50:45 -0300

    Corrige erros

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,20 @@
-export const convertDateToString = (val: Date) => { 
-    let converted_date = val.getFullYear().toString() + '-' + val.getMonth().toString() + '-'+ val.getDay().toString();
-    console.log(converted_date);
-    return converted_date; 
+export const convertDateToString = (val: Date) => {
+    if(val) {
+        let converted_date = val.getFullYear().toString() + '-' + val.getMonth().toString() + '-'+ val.getDay().toString();
+        console.log(converted_date);
+        return converted_date;
+    }
+    return null;
 };
 
-export const convertStringToDate = (val: string) => { 
-    let splited_date = val.split('-');
-    let converted_date = new Date(parseInt(splited_date[0]), parseInt(splited_date[1]), parseInt(splited_date[2]));
-    console.log(converted_date);
-    return converted_date; 
+export const convertStringToDate = (val: string) => {
+    if(val){
+        let splited_date = val.split('-');
+        let converted_date = new Date(parseInt(splited_date[0]), parseInt(splited_date[1]), parseInt(splited_date[2]));
+        console.log(converted_date);
+        return converted_date;
+    }
+    return null; 
 };
 
 export const COMPUTER_ID = 'MICROCOMPUTADOR';
