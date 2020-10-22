34cca68a441c387353ef6238e219d24815ad326d
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-10-22 17:34:29 -0300

    Adiciona variáveis no ambiente de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:8000/',
+  status: ['Almoxarifado', 'Doação', 'Sucata', 'Usado'],
+  policies: [{title:'Fora da política', value: false}, {title:'Dentro da política', value:true}],
+  servers_options: [{title:'Incluido', value:true}, {title:'Fora', value:false}, {title:'Sem registro', value:null}],
 };
